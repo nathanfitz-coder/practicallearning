794bd68c6ab6eecf75aab72905847dd256a1ddb7
Nathan <nathanfitz@me.com>
2020-08-26 22:55:02 -0400

    Again py finished

--- CodingAssignment1/codingAssigment1.py
+++ CodingAssignment1/codingAssigment1.py
@@ -1,16 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import matplotlib.style as style
+import random
 from sklearn.neighbors import KNeighborsClassifier as knn
 from sklearn import linear_model as lm
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
-
-
-
-###############################################
-
-
-
 
 
 ################################################    
@@ -50,7 +47,7 @@ def generate_data(ntrain, ntest,nclusters = 10,p = 2, s = 1):
 ###############################################
 
 
-def plot_data(data, m1, m2):
+def plot_scatter_data(data, m1, m0):
     traindata = data
     plt.scatter(traindata[:ntrain, 0], traindata[:ntrain, 1], c = "blue", alpha=0.2, label='Class 1')
     plt.scatter(traindata[ntrain:, 0], traindata[ntrain:, 1], c = "red", alpha=0.2, label='Class 0')
@@ -65,109 +62,150 @@ def plot_data(data, m1, m2):
 ##########################################
 # Linear Regression
 
-RegModel = lm.LinearRegression()
-RegModel.fit(traindata, ytrain)
-
-Ytrain_pred_LS = RegModel.predict(traindata)
-Ytest_pred_LS = RegModel.predict(testdata)
-
-Ytrain_pred_LS = [1 if i >= 0.5 else 0 for i in Ytrain_pred_LS]
-Ytest_pred_LS = [1 if i >= 0.5 else 0 for i in Ytest_pred_LS]
-
-train_err_LS = sum(ytrain != Ytrain_pred_LS) / float(2*ntrain)
-test_err_LS = sum(ytest != Ytest_pred_LS) / float(2*ntest)
+def linear_regression(traindata, testdata, ytrain, ytest, ntrain, ntest):
+    RegModel = lm.LinearRegression()
+    RegModel.fit(traindata, ytrain)
+    
+    Ytrain_pred_LS = RegModel.predict(traindata)
+    Ytest_pred_LS = RegModel.predict(testdata)
+    
+    Ytrain_pred_LS = [1 if i >= 0.5 else 0 for i in Ytrain_pred_LS]
+    Ytest_pred_LS = [1 if i >= 0.5 else 0 for i in Ytest_pred_LS]
+    
+    train_err_LS = sum(ytrain != Ytrain_pred_LS) / float(2*ntrain)
+    test_err_LS = sum(ytest != Ytest_pred_LS) / float(2*ntest)
+    return train_err_LS, test_err_LS
 
 
 ##########################################
 # Quadratic Regression
 
-
-QuadModel = Pipeline([('poly', PolynomialFeatures(degree=2)),
-                      ('linear', lm.LinearRegression(fit_intercept=False))])
-
-
-QuadModel.fit(traindata, ytrain)
-
-Ytrain_pred_QM = QuadModel.predict(traindata)
-Ytest_pred_QM = QuadModel.predict(testdata)
-
-Ytrain_pred_QM = [1 if i >= 0.5 else 0 for i in Ytrain_pred_QM]
-Ytest_pred_QM = [1 if i >= 0.5 else 0 for i in Ytest_pred_QM]
-
-train_err_QM = sum(ytrain != Ytrain_pred_QM) / float(2*ntrain)
-test_err_QM = sum(ytest != Ytest_pred_QM) / float(2*ntest)
+def quadratic_regression(traindata, testdata, ytrain, ytest, ntrain, ntest):
+    QuadModel = Pipeline([('poly', PolynomialFeatures(degree=2)),
+                          ('linear', lm.LinearRegression(fit_intercept=False))])
+    
+    QuadModel.fit(traindata, ytrain)
+    
+    Ytrain_pred_QM = QuadModel.predict(traindata)
+    Ytest_pred_QM = QuadModel.predict(testdata)
+    
+    Ytrain_pred_QM = [1 if i >= 0.5 else 0 for i in Ytrain_pred_QM]
+    Ytest_pred_QM = [1 if i >= 0.5 else 0 for i in Ytest_pred_QM]
+    
+    train_err_QM = sum(ytrain != Ytrain_pred_QM) / float(2*ntrain)
+    test_err_QM = sum(ytest != Ytest_pred_QM) / float(2*ntest)
+    return train_err_QM, test_err_QM
 
 
 ##########################################
 
-myk = [20, 17, 15, 13, 11, 9, 7, 5, 3, 1]
-mykError = []
-
-rng = np.random.default_rng()
-trainDataShuffle = np.hstack((traindata,ytrain.reshape(-1,1)))
-rng.shuffle(trainDataShuffle)   
-foldNum = 10
-foldSize = int(np.floor(len(trainDataShuffle)/foldNum))
-foldIndex = np.zeros(trainDataShuffle.shape)
-
-for j in myk:
-    myknn = knn(n_neighbors = j)
-    error = 0
+def knn_classifier(traindata, ytrain, testdata, ytest):
+    myk = [20, 17, 15, 13, 11, 9, 7, 5, 3, 1]
+    mykError = []
     
-    for fold in range(foldNum): 
-        foldIndex[foldSize*fold:foldSize*(fold+1),:] = 1
-        trainX = trainDataShuffle[foldIndex==1].reshape(-1,3)[:,0:2]
-        trainY = trainDataShuffle[foldIndex==1].reshape(-1,3)[:,2]
-        testX = trainDataShuffle[foldIndex==0].reshape(-1,3)[:,0:2]
-        testY = trainDataShuffle[foldIndex==0].reshape(-1,3)[:,2]
+    rng = np.random.default_rng()
+    trainDataShuffle = np.hstack((traindata,ytrain.reshape(-1,1)))
+    rng.shuffle(trainDataShuffle)   
+    foldNum = 10
+    foldSize = int(np.floor(len(trainDataShuffle)/foldNum))
+    foldIndex = np.zeros(trainDataShuffle.shape)
+    
+    for j in myk:
+        myknn = knn(n_neighbors = j)
+        error = 0
         
-        myknn.fit(trainX, trainY)
-        predY = myknn.predict(testX)
-        errorKNN = np.zeros(predY.shape)
-        errorKNN[testY!=predY] = 1
-        error += sum(errorKNN)
-        foldIndex.fill(0)
-
-    error /= (trainDataShuffle.shape[0]*foldNum)
-    mykError.append(error)
-
-chosen_k = myk[np.argmin(mykError)]
-myknn = knn(n_neighbors = chosen_k)
-myknn.fit(traindata, ytrain)
-
-train_err_KNN = (1 - myknn.score(traindata, ytrain))
-test_err_KNN = (1- myknn.score(testdata, ytest))
+        for fold in range(foldNum): 
+            foldIndex[foldSize*fold:foldSize*(fold+1),:] = 1
+            trainX = trainDataShuffle[foldIndex==1].reshape(-1,3)[:,0:2]
+            trainY = trainDataShuffle[foldIndex==1].reshape(-1,3)[:,2]
+            testX = trainDataShuffle[foldIndex==0].reshape(-1,3)[:,0:2]
+            testY = trainDataShuffle[foldIndex==0].reshape(-1,3)[:,2]
+            
+            myknn.fit(trainX, trainY)
+            predY = myknn.predict(testX)
+            errorKNN = np.zeros(predY.shape)
+            errorKNN[testY!=predY] = 1
+            error += sum(errorKNN)
+            foldIndex.fill(0)
+    
+        error /= (trainDataShuffle.shape[0]*foldNum)
+        mykError.append(error)
+    
+    chosen_k = myk[np.argmin(mykError)]
+    myknn = knn(n_neighbors = chosen_k)
+    myknn.fit(traindata, ytrain)
+    
+    train_err_KNN = (1 - myknn.score(traindata, ytrain))
+    test_err_KNN = (1- myknn.score(testdata, ytest))
+    return train_err_KNN, test_err_KNN, chosen_k
 
 
 ################################################
 
-def bayes(x):
-    m1sum = np.square(m1-x)
-    m1sum = np.sum(m1sum,axis=1)
-    m1sum = np.exp(-5*m1sum/2)
-    m1sum = np.sum(m1sum)
+def bayes_classifier(traindata, testdata, ytrain, ytest, m1, m0, ntrain, ntest):
+    def bayes(x):
+        m1sum = np.square(m1-x)
+        m1sum = np.sum(m1sum,axis=1)
+        m1sum = np.exp(-5*m1sum/2)
+        m1sum = np.sum(m1sum)
+        
+        m0sum = np.square(m0-x)
+        m0sum = np.sum(m0sum,axis=1)
+        m0sum = np.exp(-5*m0sum/2)
+        m0sum = np.sum(m0sum)
+        return m1sum/m0sum
     
-    m0sum = np.square(m0-x)
-    m0sum = np.sum(m0sum,axis=1)
-    m0sum = np.exp(-5*m0sum/2)
-    m0sum = np.sum(m0sum)
-    return m1sum/m0sum
-
-Ytrain_pred_Bayes = [int(bayes(x) > 1) for x in traindata]
-Ytest_pred_Bayes = [int(bayes(x) > 1) for x in testdata]
-
-train_err_Bayes = sum(ytrain !=  Ytrain_pred_Bayes) / float(2*ntrain)
-test_err_Bayes = sum(ytest !=  Ytest_pred_Bayes) / float(2*ntest)
+    Ytrain_pred_Bayes = [int(bayes(x) > 1) for x in traindata]
+    Ytest_pred_Bayes = [int(bayes(x) > 1) for x in testdata]
+    
+    train_err_Bayes = sum(ytrain !=  Ytrain_pred_Bayes) / float(2*ntrain)
+    test_err_Bayes = sum(ytest !=  Ytest_pred_Bayes) / float(2*ntest)
+    
+    return train_err_Bayes, test_err_Bayes
 
 #################################################
 # Main Function
 
 ntrain=100
 ntest=5000
+random.seed(6380)
+classification_errors = []
+bestKs = []
 
 
+for i in range(20):
+    traindata, ytrain, testdata, ytest, m1, m0 = generate_data(ntrain, ntest, nclusters = 10, p = 2, s = 1)
+    
+    train_error, test_error = linear_regression(traindata, testdata, ytrain, ytest, ntrain, ntest)
+    classification_errors.append({'method':'linear','set':'train', 'error':train_error})
+    classification_errors.append({'method':'linear','set':'test', 'error':test_error})
+    
+    train_error, test_error = quadratic_regression(traindata, testdata, ytrain, ytest, ntrain, ntest)
+    classification_errors.append({'method':'quadratic','set':'train', 'error':train_error})
+    classification_errors.append({'method':'quadratic','set':'test', 'error':test_error})
+    
+    train_error, test_error, k = knn_classifier(traindata, ytrain, testdata, ytest)
+    classification_errors.append({'method':'knn','set':'train', 'error':train_error})
+    classification_errors.append({'method':'knn','set':'test', 'error':test_error})
+    bestKs.append(k)
+    
+    train_error, test_error = bayes_classifier(traindata, testdata, ytrain, ytest, m1, m0, ntrain, ntest)
+    classification_errors.append({'method':'bayes','set':'train', 'error':train_error})
+    classification_errors.append({'method':'bayes','set':'test', 'error':test_error})
+
+df = pd.DataFrame(classification_errors)
 
 
+sns.set()
+style.use('seaborn-poster') #sets the size of the charts
+style.use('ggplot')
 
+ax = sns.boxplot(x="method", y="error", hue="set", data=df)
+
+plt.close()
+
+
+bestKdf = pd.DataFrame(list(zip(bestKs,range(len(bestKs)))),columns =['k','index'])
+ax = sns.barplot(x='index',y='k',data=bestKdf,color='lightblue')
 
 
