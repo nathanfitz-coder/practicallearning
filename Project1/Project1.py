29932e418917bf891f06b2e6a99cd46c7ecc6e50
Unknown <nathanfitz@me.com>
2020-09-10 18:42:35 -0400

    read_csv

--- Project1/Project1.py
+++ Project1/Project1.py
@@ -1,2 +1,14 @@
 import pandas as pd
 
+data = pd.read_csv('Ames_data.csv')
+
+
+testIDs = pd.read_csv("project1_testIDs.dat")
+j <- 2
+train <- data[-testIDs[,j], ]
+test <- data[testIDs[,j], ]
+test.y <- test[, c(1, 83)]
+test <- test[, -83]
+write.csv(train,"train.csv",row.names=FALSE)
+write.csv(test, "test.csv",row.names=FALSE)
+write.csv(test.y,"test_y.csv",row.names=FALSE)
