b59ca7d2ec339b5d70b1deea788a9b36fd5f3b30
Nathan <nathanfitz@me.com>
2020-09-10 21:42:51 -0400

    test train

--- Project1/Project1.py
+++ Project1/Project1.py
@@ -1,14 +1,30 @@
 import pandas as pd
+import xgboost as xgb
 
 data = pd.read_csv('Ames_data.csv')
 
 
-testIDs = pd.read_csv("project1_testIDs.dat")
-j <- 2
-train <- data[-testIDs[,j], ]
-test <- data[testIDs[,j], ]
-test.y <- test[, c(1, 83)]
-test <- test[, -83]
-write.csv(train,"train.csv",row.names=FALSE)
-write.csv(test, "test.csv",row.names=FALSE)
-write.csv(test.y,"test_y.csv",row.names=FALSE)
+testIDs = pd.read_table("project1_testIDs.dat",header=None,sep = ' ').values
+j = 2
+
+testIDs[:,j]
+
+
+testidx = data.index.isin(testIDs[:,j])
+train = data[~testidx]
+test = data[testidx]
+
+
+
+train_x = train.iloc[:,0:82]
+train_y = train.iloc[:,82]
+
+test_x = test.iloc[:,0:82]
+test_y = test.iloc[:,82]
+
+
+pd.write_csv(train,"train.csv",row.names=FALSE)
+pd.write_csv(test, "test.csv",row.names=FALSE)
+pd.write_csv(test.y,"test_y.csv",row.names=FALSE)
+
+
