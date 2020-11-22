e15dcf981f8d814442de614c35e82bfff6bb23fe
Unknown <nathanfitz@me.com>
2020-11-22 16:29:01 -0500

    Import data

--- Project4/Recomender.py
+++ Project4/Recomender.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-
+#dataframe headers
 user_header = ["UserID","Gender","Age","Occupation","Zip-code"]
+ratings_header = ['UserID','MovieID','Rating','Timestamp']
+movies_header = ['MovieID','Title','Genres']
+
+#import data
 users = pd.read_csv('MovieData/users.dat',sep='::',names=user_header)
+ratings = pd.read_csv('MovieData/ratings.dat',sep='::',names=ratings_header)
+movies = pd.read_csv('MovieData/movies.dat',sep='::',names=movies_header)
 
+#split movie genres
+movies_genres = pd.concat([pd.Series(row['MovieID'], row['Genres'].split('|')) for _, row in movies.iterrows()]).reset_index().rename(columns={'index': 'genre', 0: 'MovieID'})
 
-
-
+tmp = ratings.head()
